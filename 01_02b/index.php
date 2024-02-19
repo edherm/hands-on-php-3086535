485bdfab09aae93f307e50005a0beab7575d9d6c
edherm <edward.herman.87@gmail.com>
2024-02-19 14:21:21 +0000

    Complete chapter 3

--- 01_02b/index.php
+++ 01_02b/index.php
@@ -1,11 +1,20 @@
 <?php
 // Use the Console / Terminal for this exercise! 
-$number = 27;
-$guess = readline( 'Guess between 1 and 30: ');
-if ( $number == $guess ) {
-  echo "Yes! You guessed correctly. \n\n";
-} else {
-  echo "Alas. Your guess is incorrect. \n\n";
+$number = rand(1, 30);
+$guess = null;
+
+while ($guess != $number) {
+  $guess = readline('Guess between 1 and 30: ');
+
+  if ($number == $guess) {
+    echo "Yes! You guessed correctly. \n\n";
+  } else if ($number < $guess) {
+    echo "Try again! You guessed too high. \n\n";
+  } else if ($number > $guess) {
+    echo "Try again! You guessed too low. \n\n";
+  } else {
+    echo "Alas. Your guess is incorrect. \n\n";
+  }
 }
 
 die();
